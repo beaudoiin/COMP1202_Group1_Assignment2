928195f5886f7eb539ef2b64bcae2b30ffec6e36
beaudoiin <141838696+beaudoiin@users.noreply.github.com>
2026-03-17 17:56:13 -0400

    Update Course.cs - Added to more properties.
    Still needs some proper comments

--- development/Course.cs
+++ development/Course.cs
@@ -1,6 +1,11 @@
 namespace assignment2 {
+    ///Besure to add tripple slash summary
     internal class Course {
         //A list of the students enrolled in a specific course
         public List<Student>? EnrolledStudents { get; }
+        //Besure to comment the two below
+        public string? CourseName { get; set; }
+        public int CreditHours { get; set; }
+
     }
 }
