9272819aecf29aeab6da354526d99209c4dddddd
beaudoiin <141838696+beaudoiin@users.noreply.github.com>
2026-03-17 13:51:30 -0400

    Add files via upload
    this file is for adding configuration settings to be loaded and saved based on user input. extra not required by assignment

---
+++ config.cs
@@ -0,0 +1,12 @@
+﻿namespace Assignment2 {
+    /// <summary>
+    /// Simple configuration file for saving the language so the user doesn't have to type it in each time.
+    /// Review instances where this is used to ensure when extending nothing is overwritten.
+    /// </summary>
+    public class Configuration {
+        public string language { get; set; } = "en";
+        public Configuration( string language = "en" ) {
+            this.language = language;
+        }
+    }
+}
