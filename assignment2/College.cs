3e14c326b5fd25234fe6de31a67ba36604164d60
beaudoiin <beaudoiin@gmail.com>
2026-03-18 01:06:38 -0400

    Added two dictionaries may update to include a better apporach

--- assignment2/College.cs
+++ assignment2/College.cs
@@ -1,5 +1,19 @@
 namespace assignment2 {
     internal class College {
-        //Insert here
+        #region /// Method 1 for storing Student/Course relationship - 2 dictionaries
+        //Good for who clearly belonds to who, bad for storing additional information.
+        //may be good to have a StudentCourseProfile as the object where information for that specific course is held. 
+        //Links a student to a course and their grade information.
+
+        //Stores the List of students per course
+        public static required Dictionary<Course, List<Student>> CourseHasWhatStudents;
+        //Stores the list of courses per student
+        public static required Dictionary<Student, List<Course>> StudentHasWhatCourses;
+        #endregion
+
+        #region /// Method 2 for storing Student/Course relationship - Array of lists
+        //Populate this because using indexes in 2d array seems dumb. Good for only once source of thruth bad for tracking specifics in my opinion.
+        #endregion
+
     }
 }
